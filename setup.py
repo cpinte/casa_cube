1e2786dd33f6b62fcce15fee854933fc68a9edd4
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2020-07-10 11:42:38 +1000

    Adding license and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='casa_cube',
+      description='Package to plot data cube from CASA in Python',
+      url='http://github.com/cpinte/casa_cube',
+      author='Christophe Pinte',
+      license='MIT',
+      packages=['casa_cube'],
+      zip_safe=False)
