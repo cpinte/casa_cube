98ecf09c341afd1f8546e1142bb8e2fd3b7de460
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2025-03-24 12:12:21 +1100

    Remiving setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='casa_cube',
-      description='Package to plot data cube from CASA in Python',
-      url='http://github.com/cpinte/casa_cube',
-      author='Christophe Pinte',
-      license='MIT',
-      packages=['casa_cube'],
-      zip_safe=False)
